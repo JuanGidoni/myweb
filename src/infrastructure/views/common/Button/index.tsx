788380a5374e082b6653d03c7d5ce31d4b683603
JuanGidoni <juan.gidoni@gmail.com>
2024-01-31 22:12:21 +0100

    02: Basic Button added

---
+++ src/infrastructure/views/common/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ButtonProps } from "../../../../domain/models/Button";
+
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button {...props}>{props.children}</button>
+    )
+}
