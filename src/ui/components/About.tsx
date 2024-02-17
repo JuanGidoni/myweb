cc5274655c4dba91cf3dc183b32763d7fee7f3c0
JuanGidoni <juan.gidoni@gmail.com>
2024-02-18 00:53:07 +0100

    Added ui in components and new components

---
+++ src/ui/components/About.tsx
@@ -0,0 +1,14 @@
+import ContentLayer from '../layers/ContentLayer'
+import Parragraph from './Parragraph/Parragraph'
+
+const About = () => {
+    return (
+        <ContentLayer type="section" className="about-text">
+            <Parragraph text="I want to help others to learn from me and try to learn from others. 😎" isBold />
+            <Parragraph text="I try to be the best version of myself both personally and professionally. I want to learn everything that is within my reach." />
+            <Parragraph text="I believe that technology is our future and that if we make good use of it we can achieve great things" />
+        </ContentLayer>
+    )
+}
+
+export default About
---
+++ src/ui/components/Services/Services.tsx
@@ -0,0 +1,18 @@
+import ContentLayer from '../../layers/ContentLayer'
+import Header from '../Header/Header'
+import List, { ListProps } from '../List/List'
+
+interface ServicesProps {
+    listItems: ListProps['items']
+}
+
+const ServicesList = ({ listItems }: ServicesProps) => {
+    return (
+        <ContentLayer type="section" className="service">
+            <Header type="H3" className="h3 service-title" title="What I'm doing" />
+            <List items={listItems} className='service-list' />
+        </ContentLayer>
+    )
+}
+
+export default ServicesList
